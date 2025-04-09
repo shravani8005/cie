3c226aa57df9be87d469e7a03de3d7df9edd409f
shravani8005/ <shravani8005@gmail.com>
2025-04-09 15:55:11 +0530

    main

---
+++ vote.java
@@ -0,0 +1,14 @@
+package cie2;
+
+public class vote {
+    public static void main(String[] args){
+        int e;
+        e=22;
+        if(e>18){
+            System.out.println("eligible to vote");
+        }
+        else{
+            System.out.println("not eligible");
+        }
+    }
+}
