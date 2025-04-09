5143b9bfdaf8552cf0b8fd744198daf54adf2b00
shravani8005/ <shravani8005@gmail.com>
2025-04-09 16:04:49 +0530

    master

---
+++ simple.java
@@ -0,0 +1,15 @@
+package cie2;
+
+public class simple {
+    public static void main(String[] args){
+        int e;
+        e=22;
+        if(e>18){
+            System.out.println("eligible to vote");
+        }
+        else{
+            System.out.println("not eligible");
+        }
+    }
+    System.out.println("this is master branch");
+}
